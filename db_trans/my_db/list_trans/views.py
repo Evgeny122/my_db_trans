b5f317dc8ea07f210ec95923d3b6d224626db5b4
Evgeny <evgenn.trufanov@yandex.ru>
2023-01-24 22:29:31 +0600

    second

--- db_trans/my_db/list_trans/views.py
+++ db_trans/my_db/list_trans/views.py
@@ -3,4 +3,4 @@ from list_trans.models import Сarrier
 
 def start_page(request):
     all_list = Сarrier.objects.all()
-    return render(request, 'start_page', {'all_list':all_list})
+    return render(request, 'index.html', {'all_list':all_list})
