5014938d8625791b888ab22fe987cfdff83a04bc
Evgeny <95162969+Evgeny122@users.noreply.github.com>
2023-01-31 19:38:11 +0600

    Delete p3 directory

--- p3/mysite/base/admin.py
+++
@@ -1,5 +0,0 @@
-from email.mime import base
-from django.contrib import admin
-from .models import News
-
-admin.site.register(News)
--- p3/mysite/base/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class News(models.Model):
-    heading = models.CharField(max_length=100)
-    all_news = models.TextField()
-    def __str__(self):
-        return self.heading
-
